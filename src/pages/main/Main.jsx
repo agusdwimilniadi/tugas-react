8a4a3e59f4c8e8dad0642e72323e02cee1e512f7
Agus Dwi <agusdwimill@gmail.com>
2021-09-07 00:15:58 +0700

    Styling

---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Button from "./components/Button"
+
+export default function Main() {
+    return (
+        <>
+            <Header/>
+            <Button/>
+        </>
+
+    )
+}
---
+++ src/pages/main/components/Button.jsx
@@ -0,0 +1,16 @@
+import style from "./Button.module.css"
+
+export default function Button () {
+    return (
+        <>
+            <div className={style.container}>
+                <button className={style.box1}>
+                    <p className={style.textDesc}>
+                        Haloooooooooooooooooooooooooooo
+                    </p>
+                </button>
+            </div>
+        </>
+    )
+
+}
