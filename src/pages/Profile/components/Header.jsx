361c7e56bddbd8ceb069e7a935591fce9d348b89
Agus Dwi <agusdwimill@gmail.com>
2021-09-06 23:36:10 +0700

    test

---
+++ src/pages/Profile/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header({text}) {
+    return (
+        <div style={{background: "grey", textAlign: "center", marginBottom:"50px", marginTop:"0"}}>
+            <h1>{text}</h1>
+        </div>
+    )
+}
