f3430d34ed3e4320cca1216390ae3abcd644c929
Agus Dwi <agusdwimill@gmail.com>
2021-09-07 12:01:52 +0700

    Benahi true false dikit bgt

--- src/pages/main/components/Button.jsx
+++ src/pages/main/components/Button.jsx
@@ -4,8 +4,8 @@ export default function Button ({data}) {
     return (
         <>
             <div className={style.container}>
-                <button className={style.box1} style={data.completed ? {} : {backgroundColor:'red'}}>
-                    <p className={style.textDesc} style={data.completed ? {} : {textDecoration:'line-through'}}>
+                <button className={style.box1} style={data.completed ? {backgroundColor:'red'} : {}}>
+                    <p className={style.textDesc} style={data.completed ? {textDecoration:'line-through'} : {}}>
                         {data.title}
                     </p>
                 </button>
