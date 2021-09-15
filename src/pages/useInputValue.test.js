b3de0963d614ef926c74e70b524786f793735d2c
Agus Dwi <agusdwimill@gmail.com>
2021-09-15 08:56:48 +0700

    Test

---
+++ src/pages/useInputValue.test.js
@@ -0,0 +1,20 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useInputValue } from './useInputValue';
+
+describe('Render', () => {
+  it('Initial value', () => {
+    const { result } = renderHook(
+      () => useInputValue('Agus DM'));
+
+    expect(result.current.value).toEqual('Agus DM');
+  });
+
+  it('Uppercase value', () => {
+    const { result } = renderHook(
+      () => useInputValue('Agus DM'));
+
+    act(() => result.current.onChange({target: {value: 'Succes update'}}));
+    expect(result.current.value).toEqual('Succes update');
+  });
+
+});
