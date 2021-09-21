d77cca38015f11cc2f800264e28659b4e029f859
Agus Dwi <agusdwimill@gmail.com>
2021-09-21 15:46:27 +0700

    1

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,22 +3,21 @@ import { combineReducers } from "redux";
 import persistStore from "redux-persist/es/persistStore";
 import persistReducer from "redux-persist/lib/persistReducer";
 import storage from "redux-persist/lib/storage";
-import dataSlice  from "./dataSlice";
+import dataSlice from "./dataSlice";
 
-const reducer  = combineReducers({
-    formData  : dataSlice
-})
+const reducer = combineReducers({
+  formData: dataSlice,
+});
 
 const persistConfig = {
-    key : 'root',
-    storage
-}
+  key: "root",
+  storage,
+};
 
-const persistedReducer = persistReducer (persistConfig, reducer)
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-const store = configureStore ({reducer : persistedReducer})
+const store = configureStore({ reducer: persistedReducer });
 
-const persistore = persistStore(store)
+const persistore = persistStore(store);
 
-
-export { store, persistore }
+export { store, persistore };
